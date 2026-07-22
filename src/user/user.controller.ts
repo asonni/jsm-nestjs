6ec31b49c96c410d56f838df731fb14b6e5eed04
Aladdin Sonni <aladdin.sonni@gmail.com>
2026-07-22 14:55:39 +0200

    feat: controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  // GET /user
+  @Get()
+  getUsers() {
+    return [
+      { id: 1, name: 'John Doe' },
+      { id: 2, name: 'Aladdin' },
+      { id: 3, name: 'Jain Doe' },
+    ];
+  }
+}
