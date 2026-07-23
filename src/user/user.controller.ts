2b1998e446bcbf86523b5309401818f7f4d3c2c9
Aladdin Sonni <aladdin.sonni@gmail.com>
2026-07-23 12:00:46 +0200

    feat: add name filter to getUsers and CRUD endpoints

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
+  private readonly users = [
+    { id: 1, name: 'John Doe' },
+    { id: 2, name: 'Aladdin' },
+    { id: 3, name: 'Jane Doe' },
+  ];
+
   // GET /user
   @Get()
-  getUsers() {
-    return [
-      { id: 1, name: 'John Doe' },
-      { id: 2, name: 'Aladdin' },
-      { id: 3, name: 'Jain Doe' },
-    ];
+  getUsers(@Query('name') name?: string) {
+    if (!name) return this.users;
+
+    return this.users.filter((user) =>
+      user.name.toLowerCase().includes(name.toLowerCase()),
+    );
+  }
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    return { id, name: 'John Doe' };
+  }
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return { data: createUserDto, message: 'User created successfully' };
+  }
+  @Put(':id')
+  updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return {
+      data: { id, ...updateUserDto },
+      message: 'User updated successfully',
+    };
   }
 }
