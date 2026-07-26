e457b30ebd41ff938c37955e35472fc14d3bb8be
Aladdin Sonni <aladdin.sonni@gmail.com>
2026-07-26 14:06:52 +0200

    feat: add ConfigModule and ApiKeyMiddleware for user routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { ApiKeyMiddleware } from './middleware/api-key.middleware';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { UserController } from './user/user.controller';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes the variables available everywhere
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ApiKeyMiddleware).forRoutes(UserController);
+  }
+}
